d89cb4d81e28cee5cd1fe11f3b2f3d3219b27f34
Lee20charly <havleecrow@gmail.com>
2024-09-17 14:45:56 +0100

    mon taf

--- app/cours_gratuit/page.js
+++ app/cours_gratuit/page.js
@@ -1,21 +1,37 @@
-
-
-
-
-
+import styles from "./section.cour.module.css";
 
 export default function Home() {
   return (
-    <div className="bg-orange-600 w-full h-screen flex flex-col justify-center items-center">
-      <div>
-        <h1 className="relative -start-[620px]"> logo</h1>
+    <section className={styles.backgroundContainer}>
+      <div className="flex flex-col justify-center items-center">
+        <div className="relative -left-[620px]">
+        <img
+        className="fit-picture"
+        src="/Logo Yellow-White.png" // Assurez-vous que le fichier existe dans le dossier `public/media/cc0-images/`
+        alt="Grapefruit slice atop a pile of other slices"
+        width={50}
+        height={25}
+      />
+          <h1 className="">logo</h1>
+        </div>
+
+        <div id="videobox">
+  <video
+    width={1000}
+    height={500}
+    id="video"
+    src="C:\Users\USER\Desktop\projet\projet-avec asseko\hisMusic2.0\app\public\femme_de_potentiel.mp4"
+    autoPlay
+    loop
+    controls
+  />
+</div>
+
+
+        <h2 className="mt-7">
+          CURRICULUM PROFESSIONNEL - UNIQUE DANS LE MONDE FRANCOPHONE - FORMATION DIPLÔMANTE
+        </h2>
       </div>
-
-        <div className="w-[1000px] h-[500px] bg-red-400">
-        <video> <p> video</p> </video>
-          </div>
-
-      <h2 className="mt-7"> CURRICULUM PROFESSIONNEL - UNIQUE DANS LE MONDE FRANCOPHONE - FORMATION DIPLÔMANTE</h2>
-    </div>
+    </section>
   );
 }
