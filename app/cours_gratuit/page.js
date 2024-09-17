7d85d0399fc147ecbcb447ae02d30c046a0e48cf
Lee20charly <havleecrow@gmail.com>
2024-09-17 11:46:20 +0100

    first commit

---
+++ app/cours_gratuit/page.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-orange-600 w-full h-screen flex flex-col justify-center items-center">
+      <div>
+        <h1 className="relative -start-[620px]"> logo</h1>
+      </div>
+
+        <div className="w-[1000px] h-[500px] bg-red-400">
+        <video> <p> video</p> </video>
+          </div>
+
+      <h2 className="mt-7"> CURRICULUM PROFESSIONNEL - UNIQUE DANS LE MONDE FRANCOPHONE - FORMATION DIPLÔMANTE</h2>
+    </div>
+  );
+}
