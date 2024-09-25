fa8c3887f148dbfbc3f7b84569d58c8eac71051f
ASSEKO <assekoazareel222@gmail.com>
2024-09-25 07:29:27 +0100

    validation des police

--- app/page.js
+++ app/page.js
@@ -9,28 +9,43 @@ import { formulaire } from "./composants/formulaire";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <>
-      <div className="body">
-        <Header />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-        <Section6 />
-      </div>
-    </>
-=======
-    <div className="body">
-      <div className="section1"></div>
-      <div className="section2"></div>
-      <div className="section3"></div>
-      <div className="section4">
-        <Section4/>
-      </div>
-      <div className="section5"></div>
-      <div className="section6"></div>
-    </div>
->>>>>>> 6a2aefdd130498357498a7c80db50c913ee2e1f1
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fasthand&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <title>pablo</title>
+      </head>
+      <body>
+        <div className="body">
+          <Header />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section5 />
+          <Section6 />
+        </div>
+      </body>
+    </html>
   );
 }
